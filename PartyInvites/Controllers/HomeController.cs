90727a4bc8d2b36148b268695f086496dcbd3b3f
cezaryluksza <cezary.luksza@gmail.com>
2018-07-28 16:44:59 +0200

    first simple app completed (with bootstrap 4.1.3)

--- PartyInvites/Controllers/HomeController.cs
+++ PartyInvites/Controllers/HomeController.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using PartyInvites.Models;
 
 namespace PartyInvites.Controllers
 {
@@ -12,12 +13,27 @@ namespace PartyInvites.Controllers
         public ViewResult Index()
         {
             int hour = DateTime.Now.Hour;
-            ViewBag.Greeting = hour < 17 ? "Good morning" : "Good afternoon";
+            ViewBag.Greeting = hour < 12 ? "Good morning" : "Good afternoon";
             return View();
         }
+        [HttpGet]
         public ViewResult RsvpForm()
         {
             return View();
+        }
+        [HttpPost]
+        public ViewResult RsvpForm(GuestResponse guestResponse)
+        {
+            if (ModelState.IsValid)
+            {
+                // TODO: Email response to the party organizer
+                return View("Thanks", guestResponse);
+            }
+            else
+            {
+                // there is a validation error
+                return View();
+            }
         }
     }
 }
